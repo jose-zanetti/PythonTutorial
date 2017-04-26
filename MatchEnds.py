5b175a9dc57bcaf82742dce13732a3fb5d9eedb2
José Zanetti Arias <Jose.Zanetti@systemverification.com>
2017-04-26 11:10:45 +0200

    Add comments

--- MatchEnds.py
+++ MatchEnds.py
@@ -1,3 +1,7 @@
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
 from _functools import reduce
 def match_ends(words):
     return reduce(lambda x, y: x + y, [1 for str in words if len(str) >= 2 and str[0] == str[- 1]])
@@ -14,6 +18,7 @@ def main():
     print('match_ends')
     test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
     test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
     test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
 
 if __name__ == '__main__':
