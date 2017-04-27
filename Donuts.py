87fe5dc0bbdc7257cc56141ddf55beb0a4434593
José Zanetti Arias <Jose.Zanetti@systemverification.com>
2017-04-27 09:00:42 +0200

    Add

--- Donuts.py
+++ Donuts.py
@@ -1,3 +1,9 @@
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
     
     return 'Number of donuts: many' if count >= 10 else 'Number of donuts: %d' %count
